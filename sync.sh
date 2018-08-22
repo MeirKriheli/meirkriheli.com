64a675849c856214575b4cf874fc5cbc350e1ef8
Meir Kriheli <mkriheli@gmail.com>
2018-08-22 11:42:21 +0300

    Change sync server

--- sync.sh
+++ sync.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-rsync -avze ssh build/ mkriheli@meirkriheli.com:/home/mkriheli/webapps/main/
-ssh mkriheli@meirkriheli.com 'setfacl -m u:apache:r-x -R ~/webapps/main'
+# rsync -avze ssh build/ mkriheli@meirkriheli.com:/home/mkriheli/webapps/main/
+# ssh mkriheli@meirkriheli.com 'setfacl -m u:apache:r-x -R ~/webapps/main'
+rsync -avze ssh build/ 142.93.228.65:/srv/meirkriheli.com/
+ssh 142.93.228.65 'chgrp -R www-data /srv/meirkriheli.com'
